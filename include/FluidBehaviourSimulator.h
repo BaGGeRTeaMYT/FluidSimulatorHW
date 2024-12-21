1bd569195758c9e99e2aad75ee19d6a94b086a26
Andrew <andrey.miclanguss@gmail.com>
2024-12-21 13:08:11 +0300

    Starting point

---
+++ include/FluidBehaviourSimulator.h
@@ -0,0 +1,192 @@
+#ifndef FluidBehaviourSimulatorHEADER
+#define FluidBehaviourSimulatorHEADER
+
+#include <array>
+#include "Field.h"
+#include "Fixed.h"
+
+template<int N, int K>
+class Simulation {
+    public:
+    
+    using Fixed = Fixed<N, K>;
+
+    Fixed rho[256];
+
+    std::array<std::array<Fixed, m>, n> p;
+    std::array<std::array<Fixed, m>, n> old_p;
+
+    struct VectorField {
+        std::array<Fixed, deltas.size()> v[N][M];
+        Fixed &add(int x, int y, int dx, int dy, Fixed dv) {
+            return get(x, y, dx, dy) += dv;
+        }
+
+        Fixed &get(int x, int y, int dx, int dy) {
+            size_t i = ranges::find(deltas, pair(dx, dy)) - deltas.begin();
+            assert(i < deltas.size());
+            return v[x][y][i];
+        }
+    };
+
+    VectorField velocity{}, velocity_flow{};
+
+    std::array<std::array<int, m>, n> last_use;
+    int UT = 0;
+
+    std::tuple<Fixed, bool, std::pair<int, int>> propagate_flow(int x, int y, Fixed lim) {
+        last_use[x][y] = UT - 1;
+        Fixed ret = 0;
+        for (auto [dx, dy] : deltas) {
+            int nx = x + dx, ny = y + dy;
+            if (field[nx][ny] != '#' && last_use[nx][ny] < UT) {
+                auto cap = velocity.get(x, y, dx, dy);
+                auto flow = velocity_flow.get(x, y, dx, dy);
+                if (flow == cap) {
+                    continue;
+                }
+                // assert(v >= velocity_flow.get(x, y, dx, dy));
+                auto vp = min(lim, cap - flow);
+                if (last_use[nx][ny] == UT - 1) {
+                    velocity_flow.add(x, y, dx, dy, vp);
+                    last_use[x][y] = UT;
+                    // cerr << x << " " << y << " -> " << nx << " " << ny << " " << vp << " / " << lim << "\n";
+                    return {vp, 1, {nx, ny}};
+                }
+                auto [t, prop, end] = propagate_flow(nx, ny, vp);
+                ret += t;
+                if (prop) {
+                    velocity_flow.add(x, y, dx, dy, t);
+                    last_use[x][y] = UT;
+                    // cerr << x << " " << y << " -> " << nx << " " << ny << " " << t << " / " << lim << "\n";
+                    return {t, prop && end != pair(x, y), end};
+                }
+            }
+        }
+        last_use[x][y] = UT;
+        return {ret, 0, {0, 0}};
+    }
+
+    Fixed random01() {
+        return Fixed::from_raw((rnd() & ((1 << k) - 1)));
+    }
+
+    void propagate_stop(int x, int y, bool force = false) {
+        if (!force) {
+            bool stop = true;
+            for (auto [dx, dy] : deltas) {
+                int nx = x + dx, ny = y + dy;
+                if (field[nx][ny] != '#' && last_use[nx][ny] < UT - 1 && velocity.get(x, y, dx, dy) > 0) {
+                    stop = false;
+                    break;
+                }
+            }
+            if (!stop) {
+                return;
+            }
+        }
+        last_use[x][y] = UT;
+        for (auto [dx, dy] : deltas) {
+            int nx = x + dx, ny = y + dy;
+            if (field[nx][ny] == '#' || last_use[nx][ny] == UT || velocity.get(x, y, dx, dy) > 0) {
+                continue;
+            }
+            propagate_stop(nx, ny);
+        }
+    }
+
+    Fixed move_prob(int x, int y) {
+        Fixed sum = 0;
+        for (size_t i = 0; i < deltas.size(); ++i) {
+            auto [dx, dy] = deltas[i];
+            int nx = x + dx, ny = y + dy;
+            if (field[nx][ny] == '#' || last_use[nx][ny] == UT) {
+                continue;
+            }
+            auto v = velocity.get(x, y, dx, dy);
+            if (v < 0) {
+                continue;
+            }
+            sum += v;
+        }
+        return sum;
+    }
+
+    struct ParticleParams {
+        char type;
+        Fixed cur_p;
+        std::array<Fixed, deltas.size()> v;
+        void swap_with(int x, int y) {
+            std::swap(field[x][y], type);
+            std::swap(p[x][y], cur_p);
+            std::swap(velocity.v[x][y], v);
+        }
+    };
+
+    bool propagate_move(int x, int y, bool is_first) {
+        last_use[x][y] = UT - is_first;
+        bool ret = false;
+        int nx = -1, ny = -1;
+        do {
+            std::array<Fixed, deltas.size()> tres;
+            Fixed sum = 0;
+            for (size_t i = 0; i < deltas.size(); ++i) {
+                auto [dx, dy] = deltas[i];
+                int nx = x + dx, ny = y + dy;
+                if (field[nx][ny] == '#' || last_use[nx][ny] == UT) {
+                    tres[i] = sum;
+                    continue;
+                }
+                auto v = velocity.get(x, y, dx, dy);
+                if (v < 0) {
+                    tres[i] = sum;
+                    continue;
+                }
+                sum += v;
+                tres[i] = sum;
+            }
+
+            if (sum == 0) {
+                break;
+            }
+
+            Fixed p = random01() * sum;
+            size_t d = std::ranges::upper_bound(tres, p) - tres.begin();
+
+            auto [dx, dy] = deltas[d];
+            nx = x + dx;
+            ny = y + dy;
+            assert(velocity.get(x, y, dx, dy) > 0 && field[nx][ny] != '#' && last_use[nx][ny] < UT);
+
+            ret = (last_use[nx][ny] == UT - 1 || propagate_move(nx, ny, false));
+        } while (!ret);
+        last_use[x][y] = UT;
+        for (size_t i = 0; i < deltas.size(); ++i) {
+            auto [dx, dy] = deltas[i];
+            int nx = x + dx, ny = y + dy;
+            if (field[nx][ny] != '#' && last_use[nx][ny] < UT - 1 && velocity.get(x, y, dx, dy) < 0) {
+                propagate_stop(nx, ny);
+            }
+        }
+        if (ret) {
+            if (!is_first) {
+                ParticleParams pp{};
+                pp.swap_with(x, y);
+                pp.swap_with(nx, ny);
+                pp.swap_with(x, y);
+            }
+        }
+        return ret;
+    }
+
+    std::array<std::array<int, m>, n> dirs = {};
+
+    friend class Fixed;
+    private:
+    constexpr Field m_field;
+    int n, m;
+    mt19937 rnd(1337);
+    constexpr std::array<std::pair<int, int>, 4> deltas{{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}};
+};
+
+#endif // FluidBehaviourSimulatorHEADER
---
+++ src/Fixed.tpp
@@ -0,0 +1,5 @@
+#include "../include/Fixed.h"
+
+
+
+#undef prefix
